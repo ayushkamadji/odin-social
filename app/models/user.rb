6a9e390a68398b7479e67aebb9765b4e2ea97084
Ayushka Partohap <partohap.ayushka@gmail.com>
2017-05-13 12:47:18 +0700

    Added facebook omniauth to devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+
+
+  class << self
+    def from_omniauth(auth)
+      email = auth.info.email
+      provider = auth.provider
+      uid = auth.uid
+
+      where(provider: provider, uid: uid).first_or_create do |user|
+        user.email = email
+        user.provider = provider
+        user.uid = uid
+        user.password = Devise.friendly_token[0,20]
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512081751) do
+ActiveRecord::Schema.define(version: 20170513051256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,10 @@ ActiveRecord::Schema.define(version: 20170512081751) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 

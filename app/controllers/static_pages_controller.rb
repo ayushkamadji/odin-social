3b39079f668400574ea61d3af5b2561a15dc6665
Ayushka Partohap <partohap.ayushka@gmail.com>
2017-05-23 14:36:13 +0700

    Brand icon now redirects depending on sign in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < Devise::RegistrationsController
   def home
-    new do |user|
-      user.build_profile
+    unless signed_in?
+      new do |user|
+        user.build_profile
+      end
+    else
+      redirect_to current_user and return
     end
   end
 end
